12652ce3ec2f4703d349a8600a6677661e78aa3c
DangVu <nguyendangvu137@gmail.com>
2023-05-08 17:48:48 +0700

    fix transfer

--- src/components/WriteContracts.jsx
+++ src/components/WriteContracts.jsx
@@ -5,23 +5,21 @@ import abiBSC from '../abiBSC.json'
 import Web3 from 'web3';
 export default function WriteContracts() {
     const Tx = require("ethereumjs-tx")
+    // const Accounts = require('web3-eth-accounts')
     const INFURA_ID = "9bab56a381cb440eb809f56e01c59de5";
     const provider = new ethers.providers.JsonRpcProvider(
         `https://mainnet.infura.io/v3/${INFURA_ID}`
     );
-    // const web3Provider = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`))
     const web3Provider = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545'));
-    // console.log(provider);
     console.log(web3Provider);
     const [show, setShow] = useState(false);
     const [addressAccount, setAddressAccount] = useState('')
-    const [hashTransfer,setHashTransfer] = useState('');
-    const [success,setSuccess] = useState(false)
+    const [hashTransfer, setHashTransfer] = useState('');
+    const [success, setSuccess] = useState(false)
     const hideWallet = () => {
         setShow(!show)
     }
     async function activateInjectedProvider(providerName) {
-        // const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/9bab56a381cb440eb809f56e01c59de5"))
         const { ethereum } = window;
         if (!ethereum?.providers) {
             alert(`No ${providerName} provider found`);
@@ -50,6 +48,7 @@ export default function WriteContracts() {
         try {
             const account = await provider.request({ method: 'eth_requestAccounts' });
             console.log(account.privateKey);
+            console.log(account);
             setAddressAccount(account);
 
         } catch (error) {
@@ -66,80 +65,65 @@ export default function WriteContracts() {
         }
     }
 
+    // const handleTranfer = async (e) => {
+    //     e.preventDefault();
+    //     const data = new FormData(e.target);
+    //     const tokenAddress = '0x7D19313FeC7F7E1282676FdC807FFfB668514dDA' // BUSD
+    //     const fromAddress = `${addressAccount}` // my wallet
+    //     const toAddress = `${data.get("address").trim()}` // where to send it
+    //     const privateKey1 = Buffer.from(
+    //         '8f840246dd3cddc48f07a149e8bda4b14a49e3382bf058da322b0607fabf2532',
+    //         'hex',
+    //     )
+    //     const BUSDAmount = data.get("amount");
+    //     const amount = web3Provider.utils.toWei(BUSDAmount.toString(), 'ether');
+    //     // const amount1 = web3Provider.utils.toHex(1e16)
+    //     const contract = new web3Provider.eth.Contract(abi, tokenAddress, { from: fromAddress })
+    //     const nonce = await web3Provider.eth.getTransactionCount(fromAddress)
+    //     const gasPrice = await web3Provider.eth.getGasPrice();
+    //     const rawTransaction = { "from": fromAddress, 'gasPrice': web3Provider.utils.toHex(gasPrice),'gasLimit': web3Provider.utils.toHex(500000), "to": tokenAddress, "value": "0x0", "data": contract.methods.transfer(toAddress, amount).encodeABI(),'nonce': web3Provider.utils.toHex(nonce) }
+    //     const transactionNew = new Tx(rawTransaction, { 'chain': 'ropsten' })
+    //     transactionNew.sign(privateKey1);
+    //     web3Provider.eth.sendSignedTransaction('0x' + transactionNew.serialize().toString('hex'), function (err, hash) {
+    //         if (!err){
+    //             console.log(hash);
+    //             setHashTransfer(hash)
+    //             setSuccess(true)
+    //         }
+    //         else
+    //             alert(err);
+    //     })
+
+    // }
     const handleTranfer = async (e) => {
         e.preventDefault();
         const data = new FormData(e.target);
         const tokenAddress = '0x7D19313FeC7F7E1282676FdC807FFfB668514dDA' // BUSD
         const fromAddress = `${addressAccount}` // my wallet
-        console.log(fromAddress);
         const toAddress = `${data.get("address").trim()}` // where to send it
-        const privateKey1 = Buffer.from(
-            '117a34c111edc61d0c49c548bbff4ebafaff9fddd09f0ecf1a3e8cf17e031607',
-            'hex',
-        )
         const BUSDAmount = data.get("amount");
         const amount = web3Provider.utils.toWei(BUSDAmount.toString(), 'ether');
-        // const amount1 = web3Provider.utils.toHex(1e16)
-        const contract = new web3Provider.eth.Contract(abiBSC, tokenAddress, { from: fromAddress })
+        const contract = new web3Provider.eth.Contract(abi, tokenAddress, { from: fromAddress })
         const nonce = await web3Provider.eth.getTransactionCount(fromAddress)
         const gasPrice = await web3Provider.eth.getGasPrice();
-        const rawTransaction = { "from": fromAddress, 'gasPrice': web3Provider.utils.toHex(gasPrice),'gasLimit': web3Provider.utils.toHex(500000), "to": tokenAddress, "value": "0x0", "data": contract.methods.transfer(toAddress, amount).encodeABI(),'nonce': web3Provider.utils.toHex(nonce) }
-        const transactionNew = new Tx(rawTransaction, { 'chain': 'ropsten' })
-        transactionNew.sign(privateKey1);
-        web3Provider.eth.sendSignedTransaction('0x' + transactionNew.serialize().toString('hex'), function (err, hash) {
-            if (!err){
-                console.log(hash);
-                setHashTransfer(hash)
-                setSuccess(true)
-            }
-            else
-                alert(err);
-        })
-        // const signTransaction = await web3Provider.eth.accounts.signTransaction(tx, privateKey)
-        //send signed transaction
-        // web3Provider.eth.sendSignedTransaction(signTransaction.rawTransaction, function (error, hash) {
-        //     if (error) {
-        //         console.log(error);
-        //     } else {
-        //         console.log('transaction submited', hash);
-        //     }
-        // })
+        // Send transaction using Metamask
+        const txHash = await window.ethereum.request({
+            method: 'eth_sendTransaction',
+            params: [{
+                from: fromAddress,
+                to: tokenAddress,
+                value: '0x0',
+                gasPrice: web3Provider.utils.toHex(gasPrice),
+                gas: web3Provider.utils.toHex(500000),
+                nonce: web3Provider.utils.toHex(nonce),
+                data: contract.methods.transfer(toAddress, amount).encodeABI()
+            }]
+        });
+        console.log("a");
+        console.log('Transaction hash:', txHash);
+        setHashTransfer(txHash);
+        setSuccess(true);
     }
-    // const handleTranfer = async (e) => {
-    //     e.preventDefault();
-    //     const data = new FormData(e.target);
-    //     const tokenAddress = '0x7D19313FeC7F7E1282676FdC807FFfB668514dDA' // 
-    //     const fromAddress = '0x4F91a27324Bf6298F855819960F19e2CC1627724' // my wallet
-    //     const toAddress = '0x1708e4E5A029339795EbB40957773b21B1AF16e9' // where to send it
-    //     const privateKey1 = Buffer.from(
-    //         '117a34c111edc61d0c49c548bbff4ebafaff9fddd09f0ecf1a3e8cf17e031607',
-    //         'hex',
-    //     )
-    //     console.log(privateKey1);
-    //     const amount = 10;
-    //     const nonce = await web3Provider.eth.getTransactionCount(fromAddress)
-    //     console.log(nonce);
-    //     // Tạo object giao dịch
-    //     const token = new web3Provider.eth.Contract(abi, tokenAddress);
-    //     console.log(token);
-    //     const txObject = {
-    //         nonce: web3Provider.utils.toHex(nonce),
-    //         gasLimit: web3Provider.utils.toHex(80000), // Thay đổi nếu cần
-    //         gasPrice: web3Provider.utils.toHex(10000000000), // Thay đổi nếu cần
-    //         to: tokenAddress,
-    //         data: token.methods.transfer(toAddress, amount).encodeABI()
-    //     }
-    //     // Ký và gửi giao dịch
-    //     var tx = new Tx(txObject);
-    //     tx.sign(privateKey1);
-    //     var serializedTx = tx.serialize();
-    //     web3Provider.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
-    //         if (!err)
-    //             console.log(hash);
-    //         else
-    //             console.log(err);
-    //       });
-    // }
     return (
         <div>
             <h1 className='mb-4 text-lg font-semibold'>Write Contract (BUSD)</h1>
@@ -159,7 +143,7 @@ export default function WriteContracts() {
                 <input type='text' name='address' className='w-[500px] h-[40px] border mb-3' placeholder='To address' /><br />
                 <input text='text' name='amount' className='w-[500px] h-[40px] border' placeholder='Amount to transfer' /><br />
                 <button type='submit' className='bg-black text-white my-4 w-[300px] h-[60px]'>TRANSFER</button>
-                {success&&(
+                {success && (
                     <p>You can check at: <a href={`https://testnet.bscscan.com/tx/${hashTransfer}`} rel="noreferrer" target="_blank" className='text-blue-500'>https://testnet.bscscan.com/tx/{hashTransfer}</a></p>
                 )}
             </form>
